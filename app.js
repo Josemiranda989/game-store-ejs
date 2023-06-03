6b51e7f2572cdd49dd5626da26a88bf0e33c540f
José Miranda <josemiranda989@gmail.com>
2023-06-03 02:19:31 -0300

    update css

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 
+const app = express();
+
 const port = process.env.PORT || 3000 
 
 const mainRoutes = require('./routes/main.routes')
 const gameRoutes = require('./routes/games.routes')
 const userRoutes = require('./routes/user.routes')
 
-const app = express();
-
+/* Ejs engine */
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+/* Static folder */
 app.use(express.static(path.join(__dirname, "public")));
+/* Middlewares */
 app.use(morgan("dev"));
-// use res.render to load up an ejs view file
 
+/* Routes */
 app.use(mainRoutes)
 app.use('/games', gameRoutes)
 app.use('/users', userRoutes)
 
+/* listen */
 app.listen(port, () => {
     console.log(`Server is listening in http://localhost:${port} 😵‍💫`)
 });
